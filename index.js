17dd1848e0d891ff6c0c62555376f4e4aa0ea6ab
Marcos Veron <marcosveronar@gmail.com>
2022-12-02 03:34:39 -0300

    incluimos el menu mobile y mediaQuerys para evitar que los dos menus esten visibles al mismo tiempo

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-
+// desktop menu
 const navbarEmail = document.querySelector('.navbar-email')
 const menuDesktop = document.querySelector('.desktop-menu')
 
 navbarEmail.addEventListener("click", toggleMenuDesktop)
 
 function toggleMenuDesktop() {
-    menuDesktop.classList.toggle('inactive') //intercambia la clase inactive
+    menuDesktop.classList.toggle('inactive')
 }
+
+// mobile menu 
+const burguerMenu = document.querySelector('.menu')
+const menuMobile = document.querySelector('.mobile-menu')
+
+burguerMenu.addEventListener("click", toggleMenuMobile)
+
+function toggleMenuMobile() {
+    menuMobile.classList.toggle('inactive')
+}
+
